ede408f6cf118d9b126031a805a31233debf67d7
inf0rmed <inf0rmed@new-age>
2019-10-17 23:52:37 +0530

    added-resultStat.jsp,ResultAnalysis.java - Purpose: rebase the lost code

--- PRJS/src/main/java/backend/servlet/ResultAnalysis.java
+++ PRJS/src/main/java/backend/servlet/ResultAnalysis.java
@@ -6,11 +6,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.DistinctIterable;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.session.ClientSession;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Servlet implementation class ResultAnalysis
  */
 public class ResultAnalysis extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	MongoClient mongoClient;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -18,6 +45,7 @@ public class ResultAnalysis extends HttpServlet {
     public ResultAnalysis() {
         super();
         // TODO Auto-generated constructor stub
+        
     }
 
 	/**
@@ -25,7 +53,65 @@ public class ResultAnalysis extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// Setup database connection
+		mongoClient = new MongoClient("localhost", 27017);
+		
+		// Load the result handling web page
+		String type = request.getParameter("type");
+		System.out.println("TYPE: "+type);
+		
+		// Handle get requests for year info
+		if(type.equals("year")) {
+			//
+			// INFO: Get distinct year values
+			//
+			
+			JSONArray record= new JSONArray(); 
+			
+			MongoDatabase database = mongoClient.getDatabase("test");
+			MongoCollection<Document> resultCollection = database.getCollection("result");
+			
+			DistinctIterable<Integer> resultSet = resultCollection.distinct("Year", Integer.class);
+			
+			System.out.println("[LOG] TYPE: YEAR, Getting data");
+			for(int year:resultSet) {
+				record.put(year);
+				
+				System.out.println("Year: "+year);
+			}
+			
+			response.getWriter().println(record);
+		}// end of type 'year'
+		
+		// Handle get requests for sem info
+		else if(type.equals("sem")) {
+			//
+			// INFO: Get distinct sem values
+			// REQ: Year value
+			//
+			int year = Integer.parseInt(request.getParameter("year"));
+			
+			JSONArray record= new JSONArray(); 
+			
+			MongoDatabase database = mongoClient.getDatabase("test");
+			MongoCollection<Document> resultCollection = database.getCollection("result");
+			
+			Bson filter = eq("Year", year);
+			
+			DistinctIterable<Integer> resultSet = resultCollection.distinct("Sem", filter, Integer.class);
+			
+			System.out.println("[LOG] TYPE: YEAR, Getting data");
+			for(int sem:resultSet) {
+				record.put(sem);
+				
+				System.out.println("Sem: "+sem);
+			}
+			
+			response.getWriter().println(record);
+		}
+		
 	}
 
 	/**
---
+++ PRJS/src/main/webapp/resultStat.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Student-Subject Result Analysis</title>
+<script src="jquery-1.10.2.js"></script>
+</head>
+<body>
+	<div id="container">
+        <select name="year" id="year"></select>
+        <select name="sem" id="sem"></select>
+        <select name="subject" id="subject"></select>
+        <select name="section" id="section"></select>
+
+        <input type="submit" value="Analyze Data">
+    </div>
+    <script>
+        $(document).ready(function(){
+            $.ajax({
+			 url: 'ResultAnalysis',
+			 type: 'GET',
+			 data: {
+				 'type':'year'
+			 },
+			 success: function(resp) {
+                 resp = $.parseJSON(resp);
+                 for(var i=0;i<resp.length;i++) {
+                     $("#year").append("<option value=\""+resp[i]+"\">"+resp[i]+"</option>");
+                 }
+                }
+		    });
+        });
+    </script>
+</body>
+</html>
